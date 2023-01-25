fd2fd2f77e6bb1cd4a1f086334e2b71d80c1b0a9
Alex <acastro166@gmail.com>
2023-01-24 20:50:05 -0800

    added event reducer

--- react-app/src/store/event.js
+++ react-app/src/store/event.js
@@ -1,17 +1,51 @@
-const LOAD_SERVERS = 'servers/load'
-const LOAD_ONE_SERVER = 'server/loadOne'
-const ADD_SERVER = 'server/add'
-const EDIT_SERVER = 'server/edit'
-const DELETE_SERVER = 'server/delete'
+const LOAD_ALL_SERVERS = 'servers/loadAll'
+
 
 
 
 //------------------------------   ACTIONS   ------------------------------//
-export const loadServers = (servers) => {
+export const loadAllEvents = (events) => {
     return {
-        type: LOAD_EVENTS,
-        servers
+        type: LOAD_ALL_EVENTS,
+        events
     }
 }
 
-export const 
+
+
+//------------------------------   THUNKS   ------------------------------//
+export const getAllEvents = () => async (dispatch) => {
+    const res = await fetch('/api/events', {
+        headers: { "content-Type": "application/json" }
+    });
+
+    if(res.ok) {
+        const data = await res.json();
+        dispatch(loadAllEvents(data.servers))
+    }
+}
+
+
+
+//------------------------------   REDUCER   ------------------------------//
+
+const initialState = { allEvents: {}, oneEvent: {} }
+const eventReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        case LOAD_ALL_SERVERS:
+            {
+                const newState = { allEvents: {...state.allEvents}, oneEvent: {}}
+                action.events.forEach(event => {
+                    newState.allEvents[event.id] = event;
+                });
+                return newState;
+            }
+        
+        default:
+            return state;
+
+    }
+}
+
+export default eventReducer
