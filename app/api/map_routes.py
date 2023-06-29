cac220a6d328d8ec678a188bcd355c75f8de4478
Alex <acastro166@gmail.com>
2023-06-28 17:36:11 -0700

    removed listings route from map_routes

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Listing
 import os
 from sqlalchemy import text
 
@@ -14,20 +13,20 @@ def load_map_key():
     return {'googleMapsAPIKey': key}
 
 
-@map_routes.route('/<string:lat>/<string:lng>/<string:zoom>')
-@login_required
-def get_places(lat, lng, zoom):
+# @map_routes.route('/<string:lat>/<string:lng>/<string:zoom>')
+# @login_required
+# def get_places(lat, lng, zoom):
 
-    zoom = int(zoom)
-    distance = 15
-    if zoom > 10:
-        distance = distance / pow(2, zoom - 10)
-    if zoom < 10:
-        distance = distance * pow(2, 10 - zoom)
+#     zoom = int(zoom)
+#     distance = 15
+#     if zoom > 10:
+#         distance = distance / pow(2, zoom - 10)
+#     if zoom < 10:
+#         distance = distance * pow(2, 10 - zoom)
 
 
-    clause = "SQRT(POW(69.1 * (lat - :lati),2) + POW(69.1 * (:long - lng) * COS(lat / 57.3),2)) < :d"
+#     clause = "SQRT(POW(69.1 * (lat - :lati),2) + POW(69.1 * (:long - lng) * COS(lat / 57.3),2)) < :d"
 
-    places = Listing.query.filter(text(clause)).params(lati=lat, long=lng, d=distance).all()
+#     places = Listing.query.filter(text(clause)).params(lati=lat, long=lng, d=distance).all()
 
-    return {'places': [place.to_dict() for place in places]}
+#     return {'places': [place.to_dict() for place in places]}
