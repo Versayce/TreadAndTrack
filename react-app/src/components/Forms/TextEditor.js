aba329d4e2c477ff42fd8e0d7a74a2425403ccd7
Alex <acastro166@gmail.com>
2023-07-14 17:52:43 -0700

    added spinner to forms

--- react-app/src/components/Forms/TextEditor.js
+++ react-app/src/components/Forms/TextEditor.js
@@ -1,13 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react"
+import Spinner from "../Spinner/Spinner";
 
 const TextEditor = ( props ) => {
+    const [editorLoading, setEditorLoading] = useState(true);
     const editorRef = useRef(null);
+
     const log = () => {
         if (editorRef.current) {
             console.log(editorRef.current.getContent());
         };
     };
+
+    const handleEditorInit = () => {
+        setEditorLoading(false);
+    };
+
     const {
         handleFocus,
         handleBlur,
@@ -17,25 +25,31 @@ const TextEditor = ( props ) => {
     } = props;
 
     return (
-        <Editor 
-            apiKey="0odlwwnxu0rcj0rpw4zx4vdpztu7bvb36smfhl44l4lhdlsq"
-            onInit={(e, editor) => editorRef.current = editor}
-            init={{
-                width: "93%",
-                menuBar: false,
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-                menubar: 'file edit view insert format tools table help',
-                plugins: 'autolink lists link image charmap print preview anchor table',
-                toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright |'
-                       + 'bullist numlist | outdent indent | table tabledelete | link image',
-                lists_indent_on_tab: true,
-            }}
-            // initialValue={placeholder}
-            value={value}
-            onEditorChange={onChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-        />
+        <>
+            {editorLoading ?? <Spinner/>}
+            <Editor 
+                apiKey="0odlwwnxu0rcj0rpw4zx4vdpztu7bvb36smfhl44l4lhdlsq"
+                onInit={(e, editor) => {
+                    handleEditorInit();
+                    editorRef.current = editor
+                }}
+                init={{
+                    width: "93%",
+                    menuBar: false,
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                    menubar: 'file edit view insert format tools table help',
+                    plugins: 'autolink lists link image charmap print preview anchor table',
+                    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright |'
+                        + 'bullist numlist | outdent indent | table tabledelete | link image',
+                    lists_indent_on_tab: true,
+                }}
+                // initialValue={placeholder}
+                value={value}
+                onEditorChange={onChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
+        </>
     );
 };
 
