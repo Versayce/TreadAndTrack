c1ba18e5ced3c9eeab4a75013be8a73508b48144
Alex <acastro166@gmail.com>
2023-07-19 14:08:02 -0700

    added google firebase initialization

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -13,56 +13,72 @@ import HomePage from './components/Home/HomePage';
 import EventForm from './components/Forms/EventForm/CreateEvent';
 import NavBar from './components/Home/NavBar';
 import CarForm from './components/Forms/CarForm/CreateCar';
+//Firebase Google analytics:
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+	const [loaded, setLoaded] = useState(false);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async() => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+	const firebaseConfig = {
+		apiKey: "AIzaSyCHeQMDhvQWyQJWRVGESMC8Ni1aoKwHlxU",
+		authDomain: "tread-and-track.firebaseapp.com",
+		projectId: "tread-and-track",
+		storageBucket: "tread-and-track.appspot.com",
+		messagingSenderId: "614394649059",
+		appId: "1:614394649059:web:c5a892f5c7a54e7745e8d6",
+		measurementId: "G-TX04PP9NR0"
+	};
 
-  if (!loaded) {
-    return null;
-  }
+  	const app = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
 
-  return (
-	<>
-    <BrowserRouter>
-		<Switch>
-			<Route path='/login' exact={true}>
-				<LoginForm />
-			</Route>
-			<Route path='/sign-up' exact={true}>
-				<SignUpForm />
-			</Route>
-			<ProtectedRoute path='/cars/create' exact={true}>
-				<CarForm />
-			</ProtectedRoute>
-			<ProtectedRoute path='/users/:userId' exact={true}>
-				<User />
-			</ProtectedRoute>
-			<ProtectedRoute path='/events' exact={true}>
-				<NavBar />
-				<EventList />
-			</ProtectedRoute>
-			<ProtectedRoute path='/events/create' exact={true}>
-				<EventForm />
-			</ProtectedRoute>
-			<ProtectedRoute path='/events/:eventId' exact={true}>
-				<EventPage />
-			</ProtectedRoute>
-			<Route path='/' exact={true}>
-    			<HomePage />
-			</Route>
-		</Switch>
-	<ConditonalModal />
-    </BrowserRouter>
-	</>
-  )
+	useEffect(() => {
+		(async() => {
+		await dispatch(authenticate());
+		setLoaded(true);
+		})();
+	}, [dispatch]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return (
+		<>
+		<BrowserRouter>
+			<Switch>
+				<Route path='/login' exact={true}>
+					<LoginForm />
+				</Route>
+				<Route path='/sign-up' exact={true}>
+					<SignUpForm />
+				</Route>
+				<ProtectedRoute path='/cars/create' exact={true}>
+					<CarForm />
+				</ProtectedRoute>
+				<ProtectedRoute path='/users/:userId' exact={true}>
+					<User />
+				</ProtectedRoute>
+				<ProtectedRoute path='/events' exact={true}>
+					<NavBar />
+					<EventList />
+				</ProtectedRoute>
+				<ProtectedRoute path='/events/create' exact={true}>
+					<EventForm />
+				</ProtectedRoute>
+				<ProtectedRoute path='/events/:eventId' exact={true}>
+					<EventPage />
+				</ProtectedRoute>
+				<Route path='/' exact={true}>
+					<HomePage />
+				</Route>
+			</Switch>
+		<ConditonalModal />
+		</BrowserRouter>
+		</>
+	)
 }
 
 export default App;
