2e6b978df57903204a4ea7b95be51113dc251420
Alex <acastro166@gmail.com>
2023-07-25 12:56:54 -0700

    edited maps api key route

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
 import os
-from sqlalchemy import text
 
 map_routes = Blueprint('map', __name__)
-
 
 @map_routes.route('/key', methods=['POST'])
 def load_map_key():
